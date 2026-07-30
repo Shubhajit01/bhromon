7fb229b2375d2fb4448183c28a3ad2f07c9d2f9f
Shubhajit Chatterjee <shubhajitchatterjee1997@gmail.com>
2026-07-30 19:32:08 +0530

    chore: add react compiler linting

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-import viteReact from '@vitejs/plugin-react'
+import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
+
+import babel from '@rolldown/plugin-babel'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
@@ -14,6 +16,7 @@ const config = defineConfig({
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart(),
+    babel({ presets: [reactCompilerPreset()] }),
     viteReact(),
   ],
 })
