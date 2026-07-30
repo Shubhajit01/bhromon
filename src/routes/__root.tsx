f21eb2ed50fd19552a6dfc35741e42abc3b2c1cd
Shubhajit Chatterjee <shubhajitchatterjee1997@gmail.com>
2026-07-30 20:00:57 +0530

    chore: Setup shadcn and fonts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,32 +1,39 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
 
-import appCss from '../styles.css?url'
+import appCss from '../styles/globals.css?url';
+import fontsCss from '../styles/fonts.css?url';
+
+import interFontUrl from '@fontsource-variable/inter/files/inter-latin-wght-normal.woff2?url';
+import merriweatherFontUrl from '@fontsource-variable/merriweather/files/merriweather-latin-wght-normal.woff2?url';
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
+      { charSet: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { title: 'TanStack Start Starter' },
     ],
     links: [
       {
-        rel: 'stylesheet',
-        href: appCss,
+        rel: 'preload',
+        as: 'font',
+        crossOrigin: 'anonymous',
+        href: interFontUrl,
       },
+      {
+        rel: 'preload',
+        as: 'font',
+        crossOrigin: 'anonymous',
+        href: merriweatherFontUrl,
+      },
+      { rel: 'stylesheet', href: appCss },
+      { rel: 'stylesheet', href: fontsCss },
     ],
   }),
   shellComponent: RootDocument,
-})
+});
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
@@ -50,5 +57,5 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
